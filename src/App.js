5bc103b4c704d2cbfbc431d964a0290151820d53
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2022-12-28 01:29:20 -0800

    basic word gen stage one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+function WordGenerator() {
+  // Use the useState hook to create a state variable for the generated word
+  const [generatedWord, setGeneratedWord] = useState("");
+
+  // Define an array of words to choose from
+  const words = [
+    "apple",
+    "banana",
+    "orange",
+    "grape",
+    "strawberry",
+    "watermelon",
+  ];
+
+  // Define a function that generates a random word
+  function generateWord() {
+    // Get a random index in the words array
+    const index = Math.floor(Math.random() * words.length);
+
+    // Set the generatedWord state variable to the word at the chosen index
+    setGeneratedWord(words[index]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="word-generator">
+      <h1>Word Generator</h1>
+      <button onClick={generateWord}>Generate Word</button>
+      <p>{generatedWord}</p>
     </div>
   );
 }
 
-export default App;
+export default WordGenerator;
